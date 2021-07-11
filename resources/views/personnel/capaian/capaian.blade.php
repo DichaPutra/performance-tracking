3159c371bbdc34e076eef4ceee2495c9907925fb
dichaputra <arwindra23@gmail.com>
2021-07-12 05:50:30 +0700

    progress capaian personnel

--- resources/views/personnel/capaian/capaian.blade.php
+++ resources/views/personnel/capaian/capaian.blade.php
@@ -18,15 +18,10 @@
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Capaian</h6>
 
-                    <select class="form-control form-control-sm float-right" style="width: 20%;">
-                        @for ($i = date('n'); $i > 0; $i--)
+                    <button class="btn btn-warning">{{date('F')}} {{date('Y')}}</button>
+                    <!--<input class="form-control form-control-sm float-right" type="text" readonly="" value="{{date('F')}} {{date('Y')}}">-->
 
-                        <!--                        <option>February</option>
-                                                <option>March</option>
-                                                <option>April</option>-->
-                        <option selected="" value="{{$i}}">May</option>
-                        @endfor
-                    </select>
+
 
                 </div>
 
@@ -41,45 +36,46 @@
 
 
                     <!--KPI Table-->
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead style="background-color: #F8F9FC;">
-                            <tr>
-                                <th style="width: 8%; text-align: center;">No</th>
-                                <th style="width: 52%;">KPI</th>
-                                <th>Target</th>
-                                <th>Actual</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td style="text-align: center;">1</td>
-                                <td>
-                                    <b>Return on Equity (ROE)</b><br>
-                                    Tingkat pengembalian atas modal yang telah ditanamkan
-                                </td>
-                                <td>60%</td>
-                                <td><input type="text" class="form-control"></td>
-                            </tr>
-                            <tr>
-                                <td style="text-align: center;">2</td>
-                                <td>
-                                    <b>Return on Investment (ROI)</b><br>
-                                    Tingkat pengembalian atas investasi yang telah ditanamkan
-                                </td>
-                                <td>60%</td>
-                                <td><input type="text" class="form-control"></td>
-                            </tr>
-                            <tr>
-                                <td style="text-align: center;">3</td>
-                                <td><b>Tingkat Kesehatan (Standar BUMN)</b><br>
-                                    Kinerja berdasarkan atas sekumpulan indikator Keuangan, Operasional dan Administratif sesuai standard yang ditentukan oleh BUMN. Indikator Keuangan meliputi ROE, ROI, CASH RATIO, Current Ratio, Collection Periode, Perputaran Persediaan, Perputaran Total Asset, Rasio Modal Sendiri thd Total Aktiv Sedangkan indikator operasional adalah berkaitan dengan produktivitas tanaman, rendemen, % produk berkualitas tinggi serta penjualan langsung.
-                                </td>
-                                <td>60%</td>
-                                <td><input type="text" class="form-control"></td>
-                            </tr>                          
-                        </tbody>
-                    </table>
-
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead style="background-color: #F8F9FC;">
+                                <tr>
+                                    <th style="width: 8%; text-align: center;">No</th>
+                                    <th style="width: 52%;">KPI</th>
+                                    <th>Target</th>
+                                    <th>Actual</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style="text-align: center;">1</td>
+                                    <td>
+                                        <b>Return on Equity (ROE)</b><br>
+                                        Tingkat pengembalian atas modal yang telah ditanamkan
+                                    </td>
+                                    <td>60%</td>
+                                    <td><input type="text" class="form-control"></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center;">2</td>
+                                    <td>
+                                        <b>Return on Investment (ROI)</b><br>
+                                        Tingkat pengembalian atas investasi yang telah ditanamkan
+                                    </td>
+                                    <td>60%</td>
+                                    <td><input type="text" class="form-control"></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center;">3</td>
+                                    <td><b>Tingkat Kesehatan (Standar BUMN)</b><br>
+                                        Kinerja berdasarkan atas sekumpulan indikator Keuangan, Operasional dan Administratif sesuai standard yang ditentukan oleh BUMN. Indikator Keuangan meliputi ROE, ROI, CASH RATIO, Current Ratio, Collection Periode, Perputaran Persediaan, Perputaran Total Asset, Rasio Modal Sendiri thd Total Aktiv Sedangkan indikator operasional adalah berkaitan dengan produktivitas tanaman, rendemen, % produk berkualitas tinggi serta penjualan langsung.
+                                    </td>
+                                    <td>60%</td>
+                                    <td><input type="text" class="form-control"></td>
+                                </tr>                          
+                            </tbody>
+                        </table>
+                    </div>
                     <!--<button></button>-->
                     <button class="btn btn-primary float-right">Save</button>
                 </div>
